a0357f09200946216411c4ffd64458121b6cea54
Michael Salaverry <michaels@axonize.com>
2019-04-14 09:48:27 +0300

    feat: personalization of the template

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,8 +7,8 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Blog</h1>
+    <h2>Welcome to my Blog</h2>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
